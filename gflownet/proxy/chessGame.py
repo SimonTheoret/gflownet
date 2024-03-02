7db12041ec6ca3dfeeca1251f1a11b9bbf52e4e2
rango <sidya.galakho@umontreal.ca>
2024-03-01 20:57:31 -0500

    implemented proxy

--- gflownet/proxy/chessGame.py
+++ gflownet/proxy/chessGame.py
@@ -3,13 +3,15 @@ from chess import Board, engine
 from torchtyping import TensorType
 
 from gflownet.proxy.base import Proxy
+from gflownet.utils.common import tfloat
 
 
 class Chess(Proxy):
-    def __init__(self, engine_path: str, **kwargs):
+    def __init__(self, engine_path: str, board_check: bool = False, **kwargs):
         """Creates the proxy by giving it the path to the chess engine on the
         local machine."""
         super().__init__(**kwargs)
+        self.board_check = board_check
         self.engine = engine.SimpleEngine.popen_uci(
             engine_path
         )  # the stockfish engine.
@@ -20,8 +22,12 @@ class Chess(Proxy):
         assert env is not None
         self.boards[env.id] = env.board
 
-    def __call__(self, states: TensorType["batch", 1]) -> TensorType["batch"]:  # type: ignore
-        scores = [
-            self.engine.analyse(states[i], engine.Limit(time=1))
-            for i, _ in enumerate(states)
+    def __call__(self, states: TensorType["batch", 64]) -> TensorType["batch"]:  # type: ignore
+        analyses = [
+            self.engine.analyse(self.boards[key], engine.Limit(time=1,depth=20))
+            for key in self.boards.keys()
         ]
+        scores = []
+        for analyse in analyses :
+            scores.append(analyse['score'].relative.score())
+        return tfloat(scores, device=self.device, float_type=self.float)
