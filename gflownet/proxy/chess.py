dcdffc9ef4752b267066a4462e587b3db4d78514
Simon Théorêt <simonteoret@hotmail.com>
2024-03-26 09:50:35 -0400

    avoiding overflow error

--- gflownet/proxy/chess.py
+++ gflownet/proxy/chess.py
@@ -10,17 +10,23 @@ import concurrent.futures
 from gflownet.proxy.base import Proxy
 
 
-def default_proxy_score(centipawn: int|None) ->  float:
+def default_proxy_score(centipawn: int | None) -> float:
     if centipawn is not None:
-        return 1 / (1 + 10 ** (centipawn/4))
+        try:
+            return 1 / (1 + 10 ** (abs(centipawn) / 4))
+        except OverflowError:
+            return 0.0
     else:
         return 0.0
+
 
 class Chess(Proxy):
     def __init__(self, engine_path: str, **kwargs):
         super().__init__(**kwargs)
         self.engine_path = engine_path
-        self.default_scorer = partial(self.compute_single_score, time_limit = 0.5, proxy_score = default_proxy_score)
+        self.default_scorer = partial(
+            self.compute_single_score, time_limit=0.5, proxy_score=default_proxy_score
+        )
 
     def setup(self, env=None):
         "Leggo"
@@ -37,10 +43,14 @@ class Chess(Proxy):
             executor.shutdown(wait=True, cancel_futures=False)
         return torch.tensor(list(scores))
 
-    def compute_single_score(self, state: Board, time_limit: float, proxy_score: Callable[[int|None],float]) -> float:
+    def compute_single_score(
+        self,
+        state: Board,
+        time_limit: float,
+        proxy_score: Callable[[int | None], float],
+    ) -> float:
         with SimpleEngine.popen_uci(self.engine_path) as eng:
-            centipawn = eng.analyse(state, engine.Limit(time=time_limit), info=engine.INFO_SCORE)["score"].relative.score()
+            centipawn = eng.analyse(
+                state, engine.Limit(time=time_limit), info=engine.INFO_SCORE
+            )["score"].relative.score()
         return proxy_score(centipawn)
-
-
-
