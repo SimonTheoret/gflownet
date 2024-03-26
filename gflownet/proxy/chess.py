9f79a30ec4693a6c1cbb8f3ff67fe888d7b6dcbe
Simon Théorêt <simonteoret@hotmail.com>
2024-03-25 22:43:12 -0400

    made the proxy function multithreaded

--- gflownet/proxy/chess.py
+++ gflownet/proxy/chess.py
@@ -3,28 +3,41 @@ import torch
 from chess import Board, engine
 from torch import Tensor
 from torchtyping import TensorType
+from typing import Callable
+from functools import partial
+import concurrent.futures
 
 from gflownet.proxy.base import Proxy
 
+
+def default_proxy_score(centipawn: int|None) ->  float:
+    if centipawn is not None:
+        return 1 / (1 + 10 ** (centipawn/4))
+    else:
+        return 0.0
 
 class Chess(Proxy):
     def __init__(self, engine_path: str, **kwargs):
         super().__init__(**kwargs)
         self.engine_path = engine_path
         self.boards = {}
+        self.default_scorer = partial(self.compute_single_score, time_limit = 0.5, proxy_score = default_proxy_score)
 
     def setup(self, env=None):
         assert env is not None
         self.boards[id(env)] = env.state
 
     def __call__(self, states: TensorType["batch", 64]) -> Tensor:
+        copied = states.copy()
+        copied = copied.tolist()
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(self.default_scorer, copied)
+        return torch.tensor(copied)
+
+    def compute_single_score(self, state: Board, time_limit: float, proxy_score: Callable[[int|None],float]) -> float:
         with SimpleEngine.popen_uci(self.engine_path) as eng:
-            scores = []
-            for state in states:
-                centipawn=eng.analyse(state, engine.Limit(time=1.0), info=engine.INFO_SCORE)["score"].relative.score()
-                if centipawn is not None:
-                    scores.append(1 / (1 + 10 ** (centipawn/4)))
-                else:
-                    scores.append(0)
-        return torch.tensor(scores)
+            centipawn = eng.analyse(state, engine.Limit(time=time_limit), info=engine.INFO_SCORE)["score"].relative.score()
+        return proxy_score(centipawn)
+
+
 
