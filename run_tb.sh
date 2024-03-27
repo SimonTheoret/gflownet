cfc8e390e5d0837ac99a3f0930046c324892acd2
Simon Théorêt <simonteoret@hotmail.com>
2024-03-27 17:03:29 -0400

    added new things to runner

--- run_tb.sh
+++ run_tb.sh
@@ -1,1 +1,9 @@
-python main.py gflownet=trajectorybalance env=chess proxy=chess user.logdir.root=logs logger.do.online=True
+python main.py gflownet=trajectorybalance\
+       env=chess \
+       proxy=chess \
+       user.logdir.root=logs \
+       logger.do.online=True \
+       logger.checkpoints.period=500 \
+       logger.test.period=1000 \
+       policy.forward.checkpoint=forward \
+       policy.backward.checkpoint=backward \
